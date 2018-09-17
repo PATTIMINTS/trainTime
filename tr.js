23bba0fe4ef76ce9031a698735748c583edc4213
patti mint <patseade@Pats-MacBook-Pro.local>
2018-09-16 23:07:47 -0500

    second

--- tr.js
+++ tr.js
@@ -10,47 +10,77 @@
   };
   firebase.initializeApp(config);
 
-
 var database = firebase.database();
-
-var trainName = "";
-var destination = "";
-var trainTime = 0;
-var frequency = 0;
-
+// button for adding info
 $("#addU").on("click", function(event) {
-event.preventDefault();
+  event.preventDefault();
 
-// Grab values from text boxes
-name = $("#name-input").val().trim();
-destination = $("#dest-input").val().trim();
-firstTrainTime = $("#time-input").val().trim();
-frequency = $("#frequent-input").val().trim();
+  // Grab values from text boxes
+  var name = $("#name-input").val().trim();
+  var destination = $("#dest-input").val().trim();
+  var firstTrainTime =$("#time-input").val().trim();
+  var frequency = $("#frequent-input").val().trim();
 
-// Code for push to database
-database.ref("/users").push({
+  var newTrain = {
     name: name,
     destination: destination,
     firstTrainTime: firstTrainTime,
     frequency: frequency,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
-});
-});
-database.ref("/users").on("child_added", function(snapshot){
+    dateAdded:firebase.database.ServerValue.TIMESTAMP
+     
+  };
+console.log (newTrain);
+// Code for push to database
+database.ref("/users").push(newTrain);
 
-  // storing snapshot.val() in variable
-  var snap = snapshot.val();
-  console.log(snap);
+console.log(newTrain.name);
+console.log(newTrain.destination);
+console.log(newTrain.firstTrainTime);
+console.log(newTrain.frequency);
 
-  console.log(snap.name);
-  console.log(snap.destination);
-  console.log(snap.firstTrainTime);
-  console.log(snap.frequency);
+// clears the text boxes
+$("#name-input").val("");
+$("#dest-input").val("");
+$("#time-input").val("");
+$("#frequent-input").val("");
+    
+});
+
+
+database.ref("/users").on("child_added", function(childSnapshot, prevChildKey){
+
+//console.log(childSnapshot.val());
+
+  // storing childsnapshot.val() in variable
+  //var snap = childSnapshot.val();
+  var trainName = childSnapshot.val().name;
+  var trainDest = childSnapshot.val().destination;
+  var trainTime = childSnapshot.val().firstTrainTime;
+  var trainFreq = childSnapshot.val().frequency;
+
+  console.log(trainName);
+  console.log(trainDest);
+  console.log(trainTime);
+  console.log(trainFreq);
+
+
+  $("#train").append("<tr><td>" + trainName + "</td>");
+  $("#des").append("<tr><td>" + trainDest + "</td>");
+  $("#fre").append("<tr><td>" + trainFreq + "</td>");
+  $("#nA").append("<tr><td>" + " " + "</td>");
+  $("#minA").append("<tr><td>" + " " + "</td>");
+
+ 
 
   // Change the HTML
-  $("#train").text(snap.name);
+  /*$("#train").text(snap.name);
   $("#des").text(snap.destination);
+  $("nextA").text(snap.firstTrainTime);
   $("#fre").text(snap.frequency);
+
+  //var firstT = moment.unix(trainTime).format("MM/DD/YY");  
+
+  $("#trainT").append ("<tr><td" + trainName + "></td><td" + trainDest + "></td><td" + trainTime + "></td><td" + trainFreq + "></td></tr>");*/
   
   // Handle the error
 }, function(errorObject) {
